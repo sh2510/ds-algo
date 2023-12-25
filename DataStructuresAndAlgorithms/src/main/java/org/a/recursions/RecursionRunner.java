384029d4c1c79089a19229a04822bae744147560
sh2510 <vatsa.2510@gmail.com>
2023-12-25 14:18:14 +0530

    Initial commit

---
+++ DataStructuresAndAlgorithms/src/main/java/org/a/recursions/RecursionRunner.java
@@ -0,0 +1,8 @@
+package org.a.recursions;
+
+public class RecursionRunner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from recursions runner!!");
+    }
+}
---
+++ DataStructuresAndAlgorithms/src/main/java/org/b/arrays/ArraysRunner.java
@@ -0,0 +1,7 @@
+package org.b.arrays;
+
+public class ArraysRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello from Arrays runner!!");
+    }
+}
